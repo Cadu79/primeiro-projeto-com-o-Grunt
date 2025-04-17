7633e89d77586ca956bad3cdd46ffcf3ff6a9713
eduardoprimogenito@gmail.com <eduardoprimogenito@gmail.com>
2025-04-17 17:38:47 -0300

    sorteador

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
+const { task } = require("grunt");
+
 module.exports = function(grunt) {
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
       less: {
         development: {
             files: {
-                'main.css': 'main.less'
+                'dev/styles/main.css': 'src/styles/main.less'
             }
         },
         production: {
@@ -12,49 +14,78 @@ module.exports = function(grunt) {
                 compress: true,
             },
             files: {
-                'main.min.css': 'main.less'
+                'dist/styles/main.min.css': 'src/styles/main.less'
             }
         }
       },
-      sass: {
-        options: {
-            implementation: require('sass'),
-            sourceMap: true,
-            outputStyle: 'compressed'
+      watch: {
+        less: {
+            files: ['src/styles/**/*.less'],
+            tasks: ['less:development']
+        },
+        html: {
+            files:['src/index.html'],
+            tasks: ['replace:dev']
+        }
+      },
+      replace: {
+        dev: {
+            options: {
+                patterns: [
+                    {
+                        match: 'ENDERECO_DO_CSS',
+                        replacement: './styles/main.css'
+                    }
+                ]
+            },
+            files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    src: ['src/index.html'],
+                    dest: 'dev/'
+                }
+            ]
         },
         dist: {
-            options:{
-                style: 'compressed'
+            options: {
+                patterns: [
+                    {
+                        match: 'ENDERECO_DO_CSS',
+                        replacement: './styles/main.min.css'
+                    }
+                ]
+            },
+            files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    src: ['prebuild/index.html'],
+                    dest: 'dist/'
+                }
+            ]
+        }
+      },
+      htmlmin: {
+        dist: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true,
             },
             files: {
-                'main2.css': 'main.scss'
+                'prebuild/index.html': 'src/index.html'
             }
         }
       },
-      concurrent: {
-        target: ['olaGrunt', 'less', 'sass', 'tarefaDemorada']
-      }
+      clean: ['prebuild']
     });
   
-    grunt.registerTask('olaGrunt', function() {
-        const done = this.async();
-        setTimeout(function(){
-            grunt.log.writeln('Olá Grunt está funcionando!');
-            done();
-        }, 3000);
-    });
-
-    grunt.registerTask('tarefaDemorada', function() {
-        const done = this.async();
-        setTimeout(function(){
-            grunt.log.writeln('Olá Grunt está funcionando!');
-            done();
-        }, 3000);
-    });
-
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-concurrent');
-    
-    grunt.registerTask('default', ['concurrent']);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+        
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
   };
